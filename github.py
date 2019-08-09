d962ceb7b3f1c4bd756cef17d146b6f95c3b6890
Stanley Zheng <stanley.zheng@cloudreach.com>
2019-08-08 22:06:19 -0400

    temp commit

--- github.py
+++ github.py
@@ -1,5 +1,6 @@
 import os 
 import json
+import queue
 
 import asyncio
 import aiohttp
@@ -11,17 +12,39 @@ GITHUB_BASE_URL= 'https://api.github.com/search/'
 
 def pretty_print(d): return json.dumps(d, indent=4, sort_keys=True)
 
-async def query_repositories(query):
+async def query_repositories(*args, **kwargs):
     """
     ref: https://developer.github.com/v3/search/#search-repositories
     """
-    response = await requests.get(f'{GITHUB_BASE_URL}repositories', auth=aiohttp.BasicAuth(GITHUB_USER, GITHUB_TOKEN), params={'q': query})
+    # print(args, kwargs)
+    response = await requests.get(f'{GITHUB_BASE_URL}repositories', auth=aiohttp.BasicAuth(GITHUB_USER, GITHUB_TOKEN), params=args)
     text = await response.text()
     json = await response.json()
     return response, text, json
 
+async def query_generic(URL, **args):
+    response = await requests.get(URL, auth=aiohttp.BasicAuth(GITHUB_USER, GITHUB_TOKEN), params=args)
+    text = await response.text()
+    json = await response.json()
+    return response, text, json
+
+def parse_repositories(repositories):
+    return [i.get('full_name') for i in repositories.get('items')]
+
+
+def enqueue():
+    """
+    Python Queue job Queue
+    """
+    return None
 
 if __name__ == "__main__":    
     loop = asyncio.get_event_loop()
-    r, text, res = loop.run_until_complete(query_repositories("foo"))
-    print(r.get_encoding(), json.dumps(res, indent=4, sort_keys=True))
+    for i in range(2, 3):
+        req, text, res = loop.run_until_complete(query_repositories({
+            'q': 'foo',
+            'per_page': 50,
+            'page': i 
+        }))
+        print(req.headers, req.get_encoding(), parse_repositories(res), len(res['items']))
+        import ipdb; ipdb.set_trace()
